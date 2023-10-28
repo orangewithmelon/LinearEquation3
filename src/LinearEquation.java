fb4a2e1fb06b969a096822ba63e470b9da249834
Anson Ho <ansonh6@nycstudents.net>
2023-10-27 20:33:41 -0400

    3

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -6,6 +6,17 @@ public class LinearEquation {
     int y2;
 
     public LinearEquation(int num1, int num2, int num3, int num4) {
+        x1 = num1;
+        x2 = num2;
+        y1 = num3;
+        y2 = num4;
+    }
 
+    public double distance() {
+        double x = Math.pow(x2 - x1, 2);
+        double y = Math.pow(y2 - y1, 2);
+        double total = x + y;
+        total = Math.sqrt(total);
+        return total;
     }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -8,6 +8,7 @@ public class LinearEquationLogic {
     public String coords(String coord1, String coord2) {
         return "The two points are: " + coord1 + " and " + coord2;
     }
+
     public String start() {
         System.out.println("Welcome to the linear equation calculator!");
         System.out.print("Enter your first coordinate: ");
@@ -20,6 +21,9 @@ public class LinearEquationLogic {
         String x2 = coord2.substring(1,coord2.indexOf(","));
         String y2 = coord2.substring(coord2.indexOf(",") + 2,coord2.length()-1);
         int realx1 = Integer.parseInt(x1);
-        return x1 + x2 + y1 +y2;
+        int realx2 = Integer.parseInt(x2);
+        int realy1 = Integer.parseInt(y1);
+        int realy2 = Integer.parseInt(y2);
+        LinearEquation coord = new LinearEquation(realx1, realx2, realy1, realy2);
     }
 }
