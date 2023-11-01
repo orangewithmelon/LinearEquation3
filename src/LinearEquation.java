82dae3be48fd6feadf96c5a3650038b98fbc6e5c
Anson Ho <ansonh6@nycstudents.net>
2023-10-31 21:34:44 -0400

    final commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,7 +5,7 @@ public class LinearEquation {
     int y1;
     int y2;
 
-    public LinearEquation(int num1, int num2, int num3, int num4) {
+    public LinearEquation(int num1, int num3, int num2, int num4) {
         x1 = num1;
         x2 = num2;
         y1 = num3;
@@ -17,6 +17,39 @@ public class LinearEquation {
         double y = Math.pow(y2 - y1, 2);
         double total = x + y;
         total = Math.sqrt(total);
+        total = roundedToHundredth(total);
         return total;
+    }
+
+    public double slope() {
+        double slope = (double)(y2-y1)/(x2-x1);
+        slope = roundedToHundredth(slope);
+        return slope;
+    }
+
+    public double yIntercept() {
+        double intercept = y1 - (slope() * x1);
+        intercept = roundedToHundredth(intercept);
+        return intercept;
+    }
+
+    public String equation() {
+        if (y2 == y1) {
+            return "y = " + y1;
+        }
+        return "y = " + (y2-y1) + "/" + (x2-x1) + "x + " + yIntercept();
+    }
+
+    public String coordinateForX(double x) {
+        double product = (x * slope()) + yIntercept();
+        return "The point on the line is (" + x + ", " + product + ")";
+    }
+
+    public String lineInfo() {
+        return "The two points are: (" + x1 + ", " + y1 + ")" + " and " + "(" + x2 + ", " + y2 + ")\nThe equation of the line between these points is " + equation() + "\nThe slope of this line is: " + slope() + "\nThe y-intercept of this line is: " + yIntercept() + "\nThe distance between these points is " + distance();
+    }
+
+    public double roundedToHundredth(double toRound) {
+        return Math.round(toRound*100)/100.0;
     }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -9,21 +9,58 @@ public class LinearEquationLogic {
         return "The two points are: " + coord1 + " and " + coord2;
     }
 
-    public String start() {
+    public boolean userinput() {
+        boolean again = true;
+        System.out.println();
+        System.out.print("Would you like to enter another pair of coordinates? y/n: ");
+        String input = scan.nextLine();
+        if (input.equals("n")) {
+            again = false;
+        }
+        return again;
+    }
+
+    public int parse(String num) {
+        return Integer.parseInt(num);
+    }
+
+    public void start() {
         System.out.println("Welcome to the linear equation calculator!");
-        System.out.print("Enter your first coordinate: ");
-        String coord1 = scan.nextLine();
-        System.out.print("Enter your second coordinate: ");
-        String coord2 = scan.nextLine();
-        System.out.println(coords(coord1, coord2));
-        String x1 = coord1.substring(1,coord1.indexOf(","));
-        String y1 = coord1.substring(coord1.indexOf(",") + 2,coord1.length()-1);
-        String x2 = coord2.substring(1,coord2.indexOf(","));
-        String y2 = coord2.substring(coord2.indexOf(",") + 2,coord2.length()-1);
-        int realx1 = Integer.parseInt(x1);
-        int realx2 = Integer.parseInt(x2);
-        int realy1 = Integer.parseInt(y1);
-        int realy2 = Integer.parseInt(y2);
-        LinearEquation coord = new LinearEquation(realx1, realx2, realy1, realy2);
+        boolean again = true;
+        while (again) {
+            System.out.println();
+            System.out.print("Enter your first coordinate: ");
+            String coord1 = scan.nextLine();
+            System.out.print("Enter your second coordinate: ");
+            String coord2 = scan.nextLine();
+            System.out.println();
+            String x1 = coord1.substring(1, coord1.indexOf(","));
+            String y1 = coord1.substring(coord1.indexOf(",") + 2, coord1.length() - 1);
+            String x2 = coord2.substring(1, coord2.indexOf(","));
+            String y2 = coord2.substring(coord2.indexOf(",") + 2, coord2.length() - 1);
+            int realx1 = parse(x1);
+            int realx2 = parse(x2);
+            int realy1 = parse(y1);
+            int realy2 = parse(y2);
+            if (realx1 != realx2) {
+                LinearEquation coord = new LinearEquation(realx1, realx2, realy1, realy2);
+                System.out.println(coord.lineInfo());
+                System.out.println();
+                System.out.print("Enter a value for x: ");
+                double x = scan.nextDouble();
+                System.out.println();
+                scan.nextLine();
+                System.out.print(coord.coordinateForX(x));
+                if(!userinput()) {
+                    again = false;
+                }
+            } else {
+                System.out.print("These points are on a vertical line: x = " + realx1);
+                if(!userinput()) {
+                    again = false;
+                }
+            }
+        }
+        System.out.println("Thank you for using the slope calculator, goodbye!");
     }
 }
